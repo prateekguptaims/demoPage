711d3b3685d9e700a4425a325b7b1b012e33d12e
prateekguptaims <prateekkumargupta45@gmail.com>
2023-08-12 16:32:36 +0530

    first commit

---
+++ pages/home/home.js
@@ -0,0 +1,14 @@
+import DefaultFooter from '@/components/footer/DefaultFooter'
+import DefaulHeader from '@/components/header/DefaulHeader'
+// import Header3 from '@/components/header/Header3'
+import React from 'react'
+
+export default function home() {
+  return (
+    <>
+     <DefaulHeader />
+     <h2>kjbdjh</h2>
+     <DefaultFooter/>
+       </>
+  )
+}
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Wrapper from "@/layout/wrapper";
+import Insurance from "./home/home";
+
+const MainRoot = () => {
+  return (
+    <Wrapper>
+      <Insurance />
+    </Wrapper>
+  );
+};
+
+export default MainRoot;
